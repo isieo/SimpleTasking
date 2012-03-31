2cbb2f583afeb2889ad7295a9f692d958a7ce80f
calvin <calvin@calvin-laptop.(none)>
2012-03-31 14:47:55 +0800

    base structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTasking::Application.config.secret_token = '7ad093da8c86979a4c1d2a8317e3c89698810f8b6f7cbf3f07908163d3b8af79edabf76f5a9e56406f59e02ba55f0006a90be5af62ae5b5868093364f9c8785d'
